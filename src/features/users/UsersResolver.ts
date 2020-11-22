54245a383c4da7802fc51b76de4ae270d5e070d4
mateja <mateja176@gmail.com>
2020-11-22 17:05:41 +0100

    removes userService and injects userRepository

--- src/features/users/UsersResolver.ts
+++ src/features/users/UsersResolver.ts
@@ -1,41 +1,42 @@
+import { GraphQLID } from 'graphql';
 import { Arg, Args, Authorized, Mutation, Query, Resolver } from 'type-graphql';
+import { Repository } from 'typeorm';
+import { User } from '../../entity/User';
 import { Roles } from '../../interfaces/Roles';
 import { UserNotFoundError } from './errors';
-import { UserService } from './service';
-import { User, UserInput, UsersArgs } from './types';
+import { UserInput, UsersArgs } from './types';
 
 @Resolver(User)
 export class UserResolver {
-  constructor(private userService: UserService) {}
+  constructor(private userRepository: Repository<User>) {}
 
   @Query(() => User)
   async user(@Arg('id') id: string) {
-    const user = await this.userService.findById(id);
-    if (user === undefined) {
+    const user = await this.userRepository.findOne(id);
+    if (user) {
+      return user;
+    } else {
       throw new UserNotFoundError(id);
     }
-    return user;
   }
 
   @Query(() => [User])
   users(@Args() { skip, take }: UsersArgs) {
-    return this.userService.findAll({ skip, take });
+    return this.userRepository.find({ skip, take });
   }
 
   @Mutation(() => User)
   @Authorized()
-  addUser(@Arg('newUserData') input: UserInput): Promise<User> {
-    return this.userService.addNew(input);
+  createUser(@Arg('input') input: UserInput): Promise<User> {
+    const user = new User();
+    user.name = input.name;
+    return this.userRepository.save(user);
   }
 
-  @Mutation(() => Boolean)
+  @Mutation(() => GraphQLID)
   @Authorized(Roles.Admin)
   async removeUser(@Arg('id') id: string) {
-    try {
-      await this.userService.removeById(id);
-      return true;
-    } catch {
-      return false;
-    }
+    await this.userRepository.delete(id);
+    return id;
   }
 }
