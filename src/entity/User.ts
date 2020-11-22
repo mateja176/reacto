b7d8301de1369038e7be5bf62380a1024ad56fa2
mateja <mateja176@gmail.com>
2020-11-22 17:20:07 +0100

    replaces PrimaryGeneratedColumn with ObjectIdColumn

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,11 +1,11 @@
 import { Field, ID, ObjectType } from 'type-graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
 import { IEntity } from '../interfaces/Entity';
 
 @Entity()
 @ObjectType()
 export class User implements IEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @ObjectIdColumn({ unique: true })
   @Field(() => ID)
   id: string;
   @Column()
