7eec0bac49f00d2fd12b0b33435d6ba568182a88
mateja <mateja176@gmail.com>
2020-11-24 00:08:09 +0100

    fixes auth error

--- src/utils/authChecker.ts
+++ src/utils/authChecker.ts
@@ -2,7 +2,7 @@ import { AuthChecker } from 'type-graphql';
 import { Context } from '../interfaces/interfaces';
 
 const authChecker: AuthChecker<Context> = ({ context: { user } }, roles) => {
-  return !!user && roles.includes(user.role);
+  return !!user && (roles.length === 0 || roles.includes(user.role));
 };
 
 export default authChecker;
