9ec23dee55df7011e7845e64b937288aeb016e0d
mateja <mateja176@gmail.com>
2020-12-05 01:46:33 +0100

    adds app env vars

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,6 +6,7 @@ dotenv.config();
 if (!process.env.JWT_SECRET) {
   throw new EnvError('JWT_SECRET');
 }
+
 if (!process.env.MAIL_GUN_API_KEY) {
   throw new EnvError('MAIL_GUN_API_KEY');
 }
@@ -13,10 +14,19 @@ if (!process.env.MAIL_GUN_USERNAME) {
   throw new EnvError('MAIL_GUN_USERNAME');
 }
 
+if (!process.env.APP_ORIGIN) {
+  throw new EnvError('APP_ORIGIN');
+}
+if (!process.env.APP_EMAIL_PATH) {
+  throw new EnvError('APP_EMAIL_PATH');
+}
+
 const env = {
   jwtSecret: process.env.JWT_SECRET,
   mailGunApiKey: process.env.MAIL_GUN_API_KEY,
   mailGunUsername: process.env.MAIL_GUN_USERNAME,
+  appOrigin: process.env.APP_ORIGIN,
+  appEmailPath: process.env.APP_EMAIL_PATH,
 };
 
 export default env;
