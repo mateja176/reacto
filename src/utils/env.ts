96751a77b28134179b772e0013c14fb090e4d649
mateja <mateja176@gmail.com>
2020-12-05 01:10:30 +0100

    adds mailing env vars

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,9 +6,17 @@ dotenv.config();
 if (!process.env.JWT_SECRET) {
   throw new EnvError('JWT_SECRET');
 }
+if (!process.env.MAIL_GUN_API_KEY) {
+  throw new EnvError('MAIL_GUN_API_KEY');
+}
+if (!process.env.MAIL_GUN_USERNAME) {
+  throw new EnvError('MAIL_GUN_USERNAME');
+}
 
 const env = {
   jwtSecret: process.env.JWT_SECRET,
+  mailGunApiKey: process.env.MAIL_GUN_API_KEY,
+  mailGunUsername: process.env.MAIL_GUN_USERNAME,
 };
 
 export default env;
