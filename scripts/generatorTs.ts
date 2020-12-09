4b72ccb4469cebc200323ab011a1f39086fdc888
mateja <mateja176@gmail.com>
2020-12-09 01:48:23 +0100

    reads types from graphql file

--- scripts/generatorTs.ts
+++ scripts/generatorTs.ts
@@ -1,83 +1,97 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
+import { buildSchema } from 'graphql';
 import { join } from 'path';
 import prettier from 'prettier';
 import ts from 'typescript';
 import prettierOptions from '../.prettierrc.json';
 
-const importClause = ts.factory.createImportDeclaration(
-  [],
-  [],
-  ts.factory.createImportClause(
-    true,
-    undefined,
-    ts.factory.createNamedImports([
-      ts.factory.createImportSpecifier(
+const generatedPath = join(__dirname, '..', 'src', 'generated');
+
+(async () => {
+  const types = Object.entries(
+    buildSchema(
+      await fs.readFile(join(generatedPath, 'schema.graphql'), {
+        encoding: 'utf-8',
+      }),
+    ).getTypeMap(),
+  );
+  console.log(types);
+
+  const importClause = ts.factory.createImportDeclaration(
+    [],
+    [],
+    ts.factory.createImportClause(
+      true,
+      undefined,
+      ts.factory.createNamedImports([
+        ts.factory.createImportSpecifier(
+          undefined,
+          ts.factory.createIdentifier('IResolverObject'),
+        ),
+      ]),
+    ),
+    ts.factory.createStringLiteral('apollo-server-express'),
+  );
+
+  const interfaceDeclaration = ts.factory.createInterfaceDeclaration(
+    [],
+    [],
+    'Test',
+    [],
+    [],
+    [
+      ts.factory.createPropertySignature(
+        [],
+        'a',
         undefined,
-        ts.factory.createIdentifier('IResolverObject'),
+        ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
       ),
+      ts.factory.createPropertySignature(
+        [],
+        'b',
+        undefined,
+        ts.factory.createFunctionTypeNode(
+          [],
+          [],
+          ts.factory.createTypeReferenceNode('Test'),
+        ),
+      ),
+    ],
+  );
+
+  const exportDeclaration = ts.factory.createExportDeclaration(
+    [],
+    [],
+    true,
+    ts.factory.createNamedExports([
+      ts.factory.createExportSpecifier(undefined, 'Test'),
     ]),
-  ),
-  ts.factory.createStringLiteral('apollo-server-express'),
-);
+  );
 
-const interfaceDeclaration = ts.factory.createInterfaceDeclaration(
-  [],
-  [],
-  'Test',
-  [],
-  [],
-  [
-    ts.factory.createPropertySignature(
-      [],
-      'a',
-      undefined,
-      ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
-    ),
-    ts.factory.createPropertySignature(
-      [],
-      'b',
-      undefined,
-      ts.factory.createFunctionTypeNode(
-        [],
-        [],
-        ts.factory.createTypeReferenceNode('Test'),
-      ),
-    ),
-  ],
-);
+  const prog = ts.factory.createNodeArray([
+    importClause,
+    interfaceDeclaration,
+    exportDeclaration,
+  ]);
 
-const exportDeclaration = ts.factory.createExportDeclaration(
-  [],
-  [],
-  true,
-  ts.factory.createNamedExports([
-    ts.factory.createExportSpecifier(undefined, 'Test'),
-  ]),
-);
+  const printer = ts.createPrinter();
 
-const prog = ts.factory.createNodeArray([
-  importClause,
-  interfaceDeclaration,
-  exportDeclaration,
-]);
+  const source = printer.printList(
+    ts.ListFormat.None,
+    prog,
+    ts.createSourceFile('', '', ts.ScriptTarget.Latest),
+  );
 
-const printer = ts.createPrinter();
-
-const source = printer.printList(
-  ts.ListFormat.None,
-  prog,
-  ts.createSourceFile('', '', ts.ScriptTarget.Latest),
-);
-
-fs.writeFile(
-  join(__dirname, '..', 'src', 'generated', 'graphql.ts'),
-  prettier.format(source, {
-    parser: 'typescript',
-    ...(prettierOptions as prettier.Options),
-  }),
-  (err) => {
-    if (err) {
-      console.error(err);
-    }
-  },
-);
+  fs.writeFile(
+    join(generatedPath, 'graphql.ts'),
+    prettier.format(source, {
+      parser: 'typescript',
+      ...(prettierOptions as prettier.Options),
+    }),
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+    },
+  );
+})();
