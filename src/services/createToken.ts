079b97ab7861631f66fa1cb6c62139729e37f134
mateja <mateja176@gmail.com>
2020-12-13 16:10:14 +0100

    updates createToken user param type

--- src/services/createToken.ts
+++ src/services/createToken.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 import { jwtAlgorithm } from '../config/jwt';
+import { JWTUser } from '../interfaces/JWTUser';
 import env from './env';
 
-const createToken = (user: Record<string, unknown>) => {
+const createToken = (user: JWTUser) => {
   return jwt.sign(user, env.jwtSecret, {
     algorithm: jwtAlgorithm,
     expiresIn: '2h',
