42fa33bf3d1c3d60620b70c7c1cc86bf71305adb
mateja <mateja176@gmail.com>
2020-12-13 19:23:04 +0100

    adds MapClass type

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -3,9 +3,8 @@ import { DocumentType } from '@typegoose/typegoose';
 interface Class {
   _id: string;
 }
-export const mapDoc = <C extends Class>(
-  doc: DocumentType<C>,
-): Omit<C, '_id'> & { id: C['_id'] } => {
+export type MapClass<C extends Class> = Omit<C, '_id'> & { id: C['_id'] };
+export const mapDoc = <C extends Class>(doc: DocumentType<C>): MapClass<C> => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { _id, __v, ...cls } = doc.toJSON();
   return { ...cls, id: _id };
