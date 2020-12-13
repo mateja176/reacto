e2723e9a2c4732a5526c5bb3c7b9ee1c757c676f
mateja <mateja176@gmail.com>
2020-12-13 15:56:08 +0100

    adds password sce=hema

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,20 +1,14 @@
-import {
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
-} from 'class-validator';
+import * as yup from 'yup';
 
-@ValidatorConstraint({ name: 'customText', async: false })
-export class IsPassword implements ValidatorConstraintInterface {
-  validate(text: string) {
-    return (
-      text.length > 8 &&
-      /[A-Z]/.test(text) &&
-      [/[0-9]/, /[\W\D]/].map((regex) => regex.test(text)).filter(Boolean)
-        .length >= 1
-    );
-  }
-
-  defaultMessage() {
-    return 'The password must be at least 8 characters long and must contain an upper case letter, and a number or a special character.';
-  }
-}
+export const passwordSchema = yup
+  .string()
+  .test(
+    'Password',
+    'The password must be at least 8 characters long and must contain an upper case letter, and a number or a special character.',
+    (string?: string) =>
+      !!string &&
+      string.length > 8 &&
+      /[A-Z]/.test(string) &&
+      [/[0-9]/, /[\W\D]/].map((regex) => regex.test(string)).filter(Boolean)
+        .length >= 1,
+  );
