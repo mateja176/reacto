a2fce16efd59f1f086dd4b7fcc3c47251fe7aebd
mateja <mateja176@gmail.com>
2020-12-13 15:44:55 +0100

    fixes JWTUser

--- src/interfaces/JWTUser.ts
+++ src/interfaces/JWTUser.ts
@@ -1,5 +1,5 @@
 import { Company, User } from '../generated/graphql';
 
-export interface JWTUser
-  extends Omit<User, 'company'>,
-    Pick<Company, 'id' | 'name'> {}
+export interface JWTUser extends Omit<User, 'company'> {
+  company: Pick<Company, 'id' | 'name'>;
+}
