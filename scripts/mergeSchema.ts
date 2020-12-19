4c4c28036056466ed49153864dacd9204ba8fb47
mateja <mateja176@gmail.com>
2020-12-19 01:36:23 +0100

    create custom loadFiles implementation

--- scripts/mergeSchema.ts
+++ scripts/mergeSchema.ts
@@ -1,18 +1,38 @@
-import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
-import * as fs from 'fs-extra';
+import { promises as fs } from 'fs';
 import { print } from 'graphql';
 import { join } from 'path';
 
 const srcPath = join(__dirname, '..', 'src');
 
-const typesArray = loadFilesSync(join(srcPath, 'graphql'));
+const loadFiles = async (path: string): Promise<string> => {
+  const items = await fs.readdir(path, { withFileTypes: true });
 
-const documentNode = mergeTypeDefs(typesArray);
+  const files = await Promise.all(
+    items.map((item) => {
+      if (item.isDirectory()) {
+        return loadFiles(join(path, item.name));
+      } else if (item.name.endsWith('.graphql')) {
+        return fs.readFile(join(path, item.name), { encoding: 'utf-8' });
+      } else {
+        return '';
+      }
+    }),
+  );
 
-const stringSchema = print(documentNode);
+  return files.reduce(
+    (accumulation, content) => accumulation.concat(content),
+    '',
+  );
+};
 
 (async () => {
+  const typesArray = await loadFiles(join(srcPath, 'graphql'));
+
+  const documentNode = mergeTypeDefs([typesArray]);
+
+  const stringSchema = print(documentNode);
+
   await fs.writeFile(
     join(srcPath, 'generated', 'schema.graphql'),
     stringSchema,
