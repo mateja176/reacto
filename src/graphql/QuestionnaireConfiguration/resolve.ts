6c78414d1c3c090c2efab46a2fd30a1f309dadf1
mateja <mateja176@gmail.com>
2020-12-26 00:40:12 +0100

    adds updateQuestionnaireConfiguration

--- src/graphql/QuestionnaireConfiguration/resolve.ts
+++ src/graphql/QuestionnaireConfiguration/resolve.ts
@@ -1,9 +1,16 @@
 import { mongoose } from '@typegoose/typegoose';
 import { AdminRole, Mutation, Query } from '../../generated/graphql';
-import { Forbidden, NotAuthenticatedError } from '../../utils/errors';
+import {
+  Forbidden,
+  NotAuthenticatedError,
+  NotFoundError,
+} from '../../utils/errors';
 import { filterInputSchema, ValidatedFilterInput } from '../../utils/validate';
 import { mapQuestionnaireConfiguration } from './map';
-import { createQuestionnaireConfigurationSchema } from './validate';
+import {
+  createQuestionnaireConfigurationSchema,
+  updateQuestionnaireConfigurationSchema,
+} from './validate';
 
 const questionnaireConfigurations: Query['questionnaireConfigurations'] = async (
   _,
@@ -83,6 +90,45 @@ const createQuestionnaireConfiguration: Mutation['createQuestionnaireConfigurati
   );
 };
 
+const updateQuestionnaireConfiguration: Mutation['updateQuestionnaireConfiguration'] = async (
+  _,
+  args,
+  context,
+) => {
+  await updateQuestionnaireConfigurationSchema.validateAsync(args.input);
+
+  if (!context.user) {
+    throw new NotAuthenticatedError();
+  }
+
+  if (context.user.role === AdminRole.admin) {
+    throw new Forbidden();
+  }
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  const questionnaireConfigurationDoc = await context.models.QuestionnaireConfiguration.findByIdAndUpdate(
+    { _id: args.input.id },
+    {
+      name: args.input.name,
+      type: args.input.type,
+    },
+  );
+
+  if (!questionnaireConfigurationDoc) {
+    throw new NotFoundError();
+  }
+
+  await session.commitTransaction();
+  session.endSession();
+
+  return mapQuestionnaireConfiguration(context.models)(
+    questionnaireConfigurationDoc,
+  );
+};
+
 export const questionnaireConfigurationMutation = {
   createQuestionnaireConfiguration,
+  updateQuestionnaireConfiguration,
 };
--- src/graphql/QuestionnaireConfiguration/validate.ts
+++ src/graphql/QuestionnaireConfiguration/validate.ts
@@ -1,9 +1,22 @@
 import joi from 'joi';
-import { CreateQuestionnaireConfigurationInput } from '../../generated/graphql';
+import {
+  CreateQuestionnaireConfigurationInput,
+  UpdateQuestionnaireConfigurationInput,
+} from '../../generated/graphql';
+import { idSchema } from '../../utils/validate';
+
+const questionnaireConfigurationBase: joi.SchemaMap<CreateQuestionnaireConfigurationInput> = {
+  name: joi.string().required(),
+  type: joi.string().required(),
+};
 
 export const createQuestionnaireConfigurationSchema = joi
-  .object<CreateQuestionnaireConfigurationInput>({
-    name: joi.string().required(),
-    type: joi.string().required(),
+  .object<CreateQuestionnaireConfigurationInput>(questionnaireConfigurationBase)
+  .required();
+
+export const updateQuestionnaireConfigurationSchema = joi
+  .object<UpdateQuestionnaireConfigurationInput>({
+    ...questionnaireConfigurationBase,
+    id: idSchema,
   })
   .required();
