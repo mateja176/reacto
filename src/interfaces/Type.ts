d2de53ae56babb928a55833e18f87ad5c699ac91
mateja <mateja176@gmail.com>
2020-12-26 22:44:52 +0100

    adds interfaces/Types

---
+++ src/interfaces/Type.ts
@@ -0,0 +1,12 @@
+export type QuestionType =
+  | 'boolean'
+  | 'string'
+  | 'strings'
+  | 'multiStrings'
+  | 'number'
+  | 'numbers'
+  | 'multiNumbers'
+  | 'file'
+  | 'files';
+
+export type AnswerType = QuestionType;
