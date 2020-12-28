15251880dd290ba350407f6213b1ad897ed95b55
mateja <mateja176@gmail.com>
2020-12-28 22:00:19 +0100

    adds question pre save hook

--- src/classes/Question/Question.ts
+++ src/classes/Question/Question.ts
@@ -1,8 +1,24 @@
-import { ModelOptions, prop, Ref } from '@typegoose/typegoose';
+import {
+  DocumentType,
+  ModelOptions,
+  pre,
+  prop,
+  Ref,
+} from '@typegoose/typegoose';
+import { questionTypes } from '../../interfaces/Type';
 import { AnswerClass } from '../Answer/Answer';
 import { QuestionnaireClass } from '../Questionnaire/Questionnaire';
 import { QuestionBase } from './QuestionBase';
 
+@pre('save', function (this: DocumentType<QuestionClass>) {
+  const typeCount = questionTypes.reduce(
+    (count, key) => (this[key] ? count + 1 : count),
+    0,
+  );
+  if (typeCount !== 1) {
+    throw new Error('Question may only have a single type.');
+  }
+})
 @ModelOptions({
   options: { customName: 'Question' },
   schemaOptions: { minimize: false },
