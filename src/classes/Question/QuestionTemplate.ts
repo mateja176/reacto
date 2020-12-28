84133aede8e2d05e8f0f296344023821bba209e8
mateja <mateja176@gmail.com>
2020-12-28 22:02:31 +0100

    adds question template pre save hook

--- src/classes/Question/QuestionTemplate.ts
+++ src/classes/Question/QuestionTemplate.ts
@@ -1,7 +1,23 @@
-import { ModelOptions, prop, Ref } from '@typegoose/typegoose';
+import {
+  DocumentType,
+  ModelOptions,
+  pre,
+  prop,
+  Ref,
+} from '@typegoose/typegoose';
+import { questionTypes } from '../../interfaces/Type';
 import { QuestionnaireConfigurationClass } from '../QuestionnaireConfiguration/QuestionnaireConfiguration';
 import { QuestionBase } from './QuestionBase';
 
+@pre('save', function (this: DocumentType<QuestionTemplateClass>) {
+  const typeCount = questionTypes.reduce(
+    (count, key) => (this[key] ? count + 1 : count),
+    0,
+  );
+  if (typeCount !== 1) {
+    throw new Error('Question may only have a single type.');
+  }
+})
 @ModelOptions({
   options: { customName: 'QuestionTemplateClass' },
   schemaOptions: { minimize: false },
